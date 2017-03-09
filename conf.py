ed62a5ad29de0f6b5441fc637bda2f9f9eb5e907
Senthil Kumaran <skumaran@gatech.edu>
2017-03-09 08:03:05 -0800

    Updated configuration.

--- conf.py
+++ conf.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
