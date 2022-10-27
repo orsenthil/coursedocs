22ebf6e50b98d7deefad6bd735e54488160e34dc
Senthil Kumaran <orsenthil@gmail.com>
2022-10-26 19:24:39 -0700

    Updated script

--- udemy/roblox/sec_2.9.lua
+++ udemy/roblox/sec_2.9.lua
@@ -1,5 +1,8 @@
-local myPart = game.Workspace.MyPart
+local myPart = game.Workspace.Part
 
 myPart.Transparency = 0.5
 
+local myBridge = game.Workspace.Bridge
+
+myBridge.Grass.BrickColor = BrickColor.Random()
 
